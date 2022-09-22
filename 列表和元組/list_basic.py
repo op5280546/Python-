635b5b6657165d433d6e8f6f3a9bd7a8846df0db
YeahTing <58320975+op5280546@users.noreply.github.com>
2022-09-22 13:31:48 +0800

    Update and rename none to list_basic.py

---
+++ 列表和元組/list_basic.py
@@ -0,0 +1,16 @@
+a = [1, 2, 3]
+
+b = [1, 'abc', 2.0, ['a', 'b', 'c']]
+
+# print(a)
+# print(b)
+# print(a[0])
+
+print('a[0]=', a[0], a[1], a[2], sep='-')
+
+# print(b[1:3])
+# print(b[0:5])
+
+s = 'asdfqwer'
+
+print(s[-1])
