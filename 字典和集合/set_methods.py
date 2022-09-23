1c82844355a40f17c9b087876b207f23a204a711
YeahTing <58320975+op5280546@users.noreply.github.com>
2022-09-23 09:38:58 +0800

    Add files via upload

---
+++ 字典和集合/set_methods.py
@@ -0,0 +1,27 @@
+# s = {1, 2, 3, 4}
+#
+# s.add(5)
+# s.add(5)
+#
+# print(s)
+#
+# s.remove(5)
+# # s.remove(5)
+#
+# print(s)
+#
+# a = '1234512'
+#
+# s1 = set(a)
+#
+# print(s1)
+
+
+s1 = {1, 2, 3, 4}
+s2 = {3, 4, 5, 6}
+
+print(s1 & s2)  # 交集
+print(s1 | s2)  # 全集
+print(s1 ^ s2)  # 重複去掉
+print(s1 - s2)
+print(s2 - s1)
