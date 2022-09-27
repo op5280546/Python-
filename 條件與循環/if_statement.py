e30186c6e2a5b4aa8f2748ff83a1e0bedfc2064d
YeahTing <58320975+op5280546@users.noreply.github.com>
2022-09-27 14:04:19 +0800

    Create if_statement.py

---
+++ 條件與循環/if_statement.py
@@ -0,0 +1,27 @@
+# IF
+
+# a = int(input("In:"))
+# print(a, type(a))
+
+# if a > 0:
+#     print("大於0")
+
+# if else
+
+# a = int(input("In:"))
+
+# if a > 0:
+#     print("大於0")
+# else:
+#     print('小於等於0')
+
+# if elif
+
+a = int(input("In:"))
+
+if a > 0:
+    print("大於0")
+elif a == 0:
+    print('等於0')
+else:
+    print('小於0')
